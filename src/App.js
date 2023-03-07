0f6cc2afdd1517abb6aa8a392cd88ce215cd9486
yuliussetyawan <yuliusetyawan@gmail.com>
2023-03-07 23:19:35 +0700

    Delete with events & prop drilling

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 
-function App(){
+function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
-  return(
+  const deleteFeedback = (id) => {
+    if (window.confirm("Arre you sure want to delete?")){
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+  return (
     <>
-    <Header />
-    <div className="container">
-      <FeedbackList feedback={feedback}/>
-    </div>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
     </>
-  )
-
-  
+  );
 }
 
 export default App;
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,15 @@
 import Card from './shared/Card';
 import PropTypes from 'prop-types';
+import { FaTimes } from "react-icons/fa";
 
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
+
   return (
     <Card >
       <div className="num-display">{item.rating}</div>
+      <button className='close' onClick={() => handleDelete(item.id)}>
+      <FaTimes color='purple' />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
