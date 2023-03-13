3e8c3aab81c51b32d1018e0ddcfb6e5cbadbff15
yuliussetyawan <yuliusetyawan@gmail.com>
2023-03-13 22:35:04 +0700

    Creating links

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Card from "../components/shared/Card";
 
 function AboutPage() {
   return (
     <Card>
       <div className="about">
-      <h1>About This Project</h1>
-      <p>This is react app to leave a feedback</p>
-      <p>Version: 1.0.0</p>
-      <p><a href="/">Back to Home</a></p>
-
+        <h1>About This Project</h1>
+        <p>This is react app to leave a feedback</p>
+        <p>Version: 1.0.0</p>
+        <p>
+          <Link to="/">Back to Home</Link>
+        </p>
       </div>
     </Card>
   );
